039256ee306efc1f9d3e99158f4824d642eeb561
Jeroen Gerits <jeroen@gerits.email>
2025-01-01 21:01:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# @styleshift/utils
---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  external: [
+    'react',
+    'react-dom',
+    '@radix-ui/react-accordion',
+    'tailwind-variants',
+    'clsx',
+    'lucide-react',
+    'tailwind-merge',
+  ],
+});
